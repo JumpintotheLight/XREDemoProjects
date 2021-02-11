cb94c46deda715274e01ac2ccb3a3f65be6c204c
Sky Rolnick <skymeson@gmail.com>
2021-02-11 16:42:23 -0500

    Lesson 1

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Color color;
+    private Renderer rend; 
+
+    public void Start()
+    {
+        rend = GetComponent<Renderer>(); 
+
+    }
+
+    public void Update()
+    {
+        
+    }
+
+
+    public void DoSomething()
+    {
+        rend.material.color = color;
+    }
+
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject playerPrefab; 
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SpawnPlayer()
+    {
+        if (player != null)
+            Destroy(player); 
+
+        player =  Instantiate(playerPrefab, spawnPoints[Random.Range(0, spawnPoints.Length-1)]); 
+
+    }
+}
